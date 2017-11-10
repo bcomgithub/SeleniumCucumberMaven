3e034b30aa9a5fa648836321e59ae5cf3ad8bd43
bcomgithub <32312268+bcomgithub@users.noreply.github.com>
2017-11-10 16:07:44 -0500

    update homepage url

--- src/test/java/StepDefinitions/Homepage.java
+++ src/test/java/StepDefinitions/Homepage.java
@@ -15,7 +15,7 @@ public class Homepage {
     public void I_go_to_the_home_page() {
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\" + "chromedriver.exe");
         driver = new ChromeDriver();
-        driver.navigate().to("http://www.bloomingdales.com");
+        driver.navigate().to("https://www.qa15codebloomingdales.fds.com/");
     }
 
     @When("^I search for a product$")
